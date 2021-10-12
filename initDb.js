bfa1163485280c800aea5947b6ba5bb111f6753b
jinyedo <whereyedo@naver.com>
2021-10-12 16:29:40 +0900

    DB 초기화 작업 및 더미 데이터 생성

--- initDb.js
+++ initDb.js
@@ -2,10 +2,12 @@ import db from './src/models/index.js'
 import bcrypt from 'bcrypt';
 import faker from "faker";
 
-
 const { User, Board, Permission } = db;
 
 faker.locale = "ko";
+
+const userCount = 200;
+const boardCount = userCount * 0.3 * 365;
 
 const getRandomInt = (min, max) => {
     min = Math.ceil(min);
@@ -13,38 +15,65 @@ const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-const roles = ['NON-MEMBER', 'MEMBER', 'ADMIN']
+const permissions = [
+    {title: "전체 관리자", level: 0, desc: "관리자 권한"},
+    {title: "게시판 관리자", level: 1, desc: "게시판 관리자 권한"},
+    {title: "사용자 관리자", level: 2, desc: "사용자 관리자 권한"},
+    {title: "일반 사용자", level: 3, desc: "일반 사용자 권한"},
+    {title: "게스트", level: 4, desc: "게스트 권한"},
+];
 
-/* User 데이터 생성 */
-const user_sync = async () => {
-    for (let i=0; i<100; i++) {
-        const hashPwd = await bcrypt.hash("test1234", 10);
-        const createUser = await User.create({
-            name: faker.name.lastName()+faker.name.firstName(),
-            age: getRandomInt(15,50),
-            password: hashPwd
-        });
-        await createUser.createPermission({
-            title: i < 30 ? roles[0] : (i < 60 ? roles[1] : roles[2]),
-            level: i < 30 ? 2 : (i < 60 ? 1 : 2)
-        })
+const permission_sync = async () => {
+    try {
+        for (let i=0; i<permissions.length; i++) {
+            const { title, level, desc } = permissions[i];
+            await Permission.create({ title, level, desc });
+        }
+    } catch(err) {
+        console.log(err);
     }
 }
 
+/* User 데이터 생성 */
+const user_sync = async () => {
+    try {
+        for (let i=0; i<userCount; i++) {
+            const hashPwd = await bcrypt.hash("test1234", 10);
+            const createUser = await User.create({
+                name: faker.name.lastName()+faker.name.firstName(),
+                age: getRandomInt(15,50),
+                password: hashPwd,
+                permissionId: getRandomInt(1, permissions.length)
+            });
+            if (i%10 === 0) console.log(`createUser: ${i}/${userCount}`);
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
 
 // Board 데이터 생성 
 const board_sync = async () => {
-    for (let i=0; i<100; i++) {
-        await Board.create({
-            title: faker.lorem.sentence(1),
-            content: faker.lorem.sentence(10),
-            userId: getRandomInt(1, 1000)
-        });
+    try {
+        for (let i=0; i<boardCount; i++) {
+            await Board.create({
+                title: faker.lorem.sentence(1),
+                content: faker.lorem.sentence(10),
+                userId: getRandomInt(1, userCount)
+            });
+            if (i%10 === 0) console.log(`createBoard: ${i}/${boardCount}`);
+        }
+    } catch(err) {
+        console.log(err);
     }
 }
 
 db.sequelize.query("SET FOREIGN_KEY_CHECKS = 0", {raw: true}).then(async () => {
-    db.sequelize.sync({ force: true })
-    user_sync();
-    //board_sync();
+    await db.sequelize.sync({ force: true });
+    await permission_sync();
+    console.log("Permission 생성 완료");
+    await user_sync();
+    console.log("User 생성 완료");
+    await board_sync();
+    console.log("Board 생성 완료");
 })
