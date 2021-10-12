1af0788dfda63e64ca2f09c492058e507402699b
jinyedo <whereyedo@naver.com>
2021-10-12 14:49:42 +0900

    user 생성할 때 연관관계를 설정한 permission주기

--- initDb.js
+++ initDb.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import faker from "faker";
 
 
-const { User, Board } = db;
+const { User, Board, Permission } = db;
 
 faker.locale = "ko";
 
@@ -13,24 +13,27 @@ const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+const roles = ['NON-MEMBER', 'MEMBER', 'ADMIN']
 
 /* User 데이터 생성 */
 const user_sync = async () => {
-    await User.sync({ force: true }); // { force: true }: User 초기화
-    for (let i=0; i<1000; i++) {
+    for (let i=0; i<100; i++) {
         const hashPwd = await bcrypt.hash("test1234", 10);
-        await User.create({
+        const createUser = await User.create({
             name: faker.name.lastName()+faker.name.firstName(),
             age: getRandomInt(15,50),
             password: hashPwd
         });
+        await createUser.createPermission({
+            title: i < 30 ? roles[0] : (i < 60 ? roles[1] : roles[2]),
+            level: i < 30 ? 2 : (i < 60 ? 1 : 2)
+        })
     }
 }
-user_sync();
+
 
 // Board 데이터 생성 
 const board_sync = async () => {
-    await Board.sync({ force: true }); // { force: true }: User 초기화
     for (let i=0; i<100; i++) {
         await Board.create({
             title: faker.lorem.sentence(1),
@@ -39,4 +42,9 @@ const board_sync = async () => {
         });
     }
 }
-board_sync();
+
+db.sequelize.query("SET FOREIGN_KEY_CHECKS = 0", {raw: true}).then(async () => {
+    db.sequelize.sync({ force: true })
+    user_sync();
+    //board_sync();
+})
